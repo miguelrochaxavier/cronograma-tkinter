49453e7565fe33d20eaf1b790c354a7c40793c80
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-10 08:57:47 -0300

    fix : implenetação humor

--- app.py
+++ app.py
@@ -10,4 +10,13 @@ CTkLabel(master=app, text="Cronograma com Tkinter", font=("Arial Bold", 20), jus
 stats_frame = CTkFrame(master=app, fg_color="transparent")
 stats_frame.pack( padx=(54, 0), pady=(18, 0), anchor="nw")
 
+btn = CTkComboBox(master=app, values=['Feliz', 'Normal', 'Bravo', 'Triste'])
+btn.place(relx=0.23, rely=0.14, anchor='center')
+
+check = CTkCheckBox(master=app, text='Teste 01', width=300)
+check.place(relx=0.4, rely=0.2, anchor='center')
+
+check = CTkCheckBox(master=app, text='Teste 02', width=300) #rely = 0.05 de distância entre um e outro.
+check.place(relx=0.4, rely=0.25, anchor='center')
+
 app.mainloop()
