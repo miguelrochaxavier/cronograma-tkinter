311765a60943a47b9317931d2a3c6cb680ac1e5b
miguelrochaxavier <miguellrochaxavier@gmail.com>
2025-03-10 08:43:50 -0300

    fix

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from customtkinter import *
 
 app = CTk()
-app.geometry('800x500')
+app.geometry('500x800')
 app.resizable(0,0)
 app.title('Cronograma')
 
-CTkLabel(master=app, text="Calculadora Fatorial com Python e Tkinter", font=("Arial Bold", 20), justify="left").pack(anchor="w", pady=(43, 18), padx=(56,0))
+CTkLabel(master=app, text="Cronograma com Tkinter", font=("Arial Bold", 20), justify="left").pack(anchor="w", pady=(43, 18), padx=(56,0))
 stats_frame = CTkFrame(master=app, fg_color="transparent")
 stats_frame.pack( padx=(54, 0), pady=(18, 0), anchor="nw")
+
+app.mainloop()
